997f441b03ef8d7d375df669e22585ad59d87c71
Duba Halakhe <dubahdida@gmail.com>
2025-09-26 16:04:35 +0300

    ldone

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+// 1. Function declaration
 function saturdayFun(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
 
+// 2. Function expression
 const mondayWork = function(activity = "go to the office") {
   return `This Monday, I will ${activity}.`;
 };
 
+// 3. Function returning a function (closure)
 function wrapAdjective(flair = "*") {
   return function(adj = "special") {
     return `You are ${flair}${adj}${flair}!`;
   };
 }
+
+// ✅ Export so CodeGrade can access them
+module.exports = {
+  saturdayFun,
+  mondayWork,
+  wrapAdjective
+};
